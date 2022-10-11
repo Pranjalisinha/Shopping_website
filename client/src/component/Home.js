598b62a31df71ce7c7cbe3cfeba3c36dc03fd7e1
pranjalisinha <pranjalisinha83@gmail.com>
2022-10-11 21:43:28 +0530

    background added

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
         <>
             <div className="home">
                 <div class="card bg-dark text-white">
-                    <img class="card-img" src="shopping_background.jpg" alt="shopping" />
+                    <img class="card-img" src="background_image.jpg" alt="shopping" />
                     <div class="card-img-overlay">
                         <h5 class="card-title display-3">FESTIVE SALE</h5>
                         <p class="card-text lead fs-2">CHECKOUT FESTIVE COLLECTION</p>
